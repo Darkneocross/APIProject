0f3d51817978cee1687ff51fd96fd44a39121a60
Aneesh Vempaty <avempaty@purdue.edu>
2016-06-22 00:44:58 -0700

    idr the changes
    aaasssssklsks

--- Public/submit_action.js
+++ Public/submit_action.js
@@ -1,9 +1,2 @@
 //printSubmittedText();
 
-function printSubmittedText() {
-	var order = document.getElementById("firstName").value;
-	var orderTwo = document.getElementById("lastName").value;
-	var orderThree = order + " " + orderTwo;
-	document.getElementById("orderThree") = orderThree;
-	//document.write(orderThree);// = orderThree;
-}
