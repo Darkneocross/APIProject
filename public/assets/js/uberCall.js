101b5847a9302813dc92c568c7db9da3b8686049
Vincent Jiang <jiangv.purdue.edu>
2016-06-29 17:04:57 -0700

    Bugfixes 2.

--- public/assets/js/uberCall.js
+++ public/assets/js/uberCall.js
@@ -1,19 +1,32 @@
 var uberClientId = "5uROnzkiqJnVpj6BWRBUmP1l00oaRYe5";
-var uberServerToken = "E6Kt0x-U0uI652EaCOLvFPXGfgSEXrNwF_ycpfu5";
+var uberServerToken = "5RKv2XQEACjqgIkbUslzevHw9wV6Ou6zuI88giiE";
+var serverTokenString = "Token " + uberServerToken;
+
 var startLatitude = "40.741549";
 var startLongitude = "-73.988991";
 var endLatitude = "40.741549";
 var endLongitude = "-73.988991";
+var timer;
+
+if (typeof timer === typeof undefined) {
+    timer = setInterval(function () {
+        getEstimatesForUserLocation(startLatitude, startLongitude);
+    }, 60000);
+    getEstimatesForUserLocation(startLatitude, startLongitude);
+}
 
 function getEstimatesForUserLocation(latitude, longitude) {
     $.ajax({
-        url: "https://api.uber.com/v1/estimates/price?start_latitude=" + startLatitude + "&start_longitude=" + startLongitude + "&end_latitude=" + endLatitude + "&end_longitude=" + endLongitude, // REMINDER:
+        /*url: "https://api.uber.com/v1/estimates/price?start_latitude=" + startLatitude + "&start_longitude=" + startLongitude + "&end_latitude=" + endLatitude + "&end_longitude=" + endLongitude + "",*/
+        url: "https://api.uber.com/v1/estimates/price?start_latitude=40.741549&start_longitude=-73.988991&end_latitude=40.741549&end_longitude=-73.988991",
+        // REMINDER:
         // Add in latitude and longitude of location to 
         // url call
         headers: {
-            Authorization: "Token " + uberServerToken
-        }
-        , success: function (result) {
+            'Authorization': serverTokenString
+        },
+        success: function (result) {
+            console.log(serverTokenString);
             console.log(JSON.stringify(result));
             /*var json = JSON.parse(result);
             var data = [];
